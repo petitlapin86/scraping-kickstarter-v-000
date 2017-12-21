b338cbde62699d943638be881bc2b0725e416435
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-21 18:50:29 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,10 +6,22 @@ require 'nokogiri'
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
+# location: project.css("ul.project-meta span.location-name").text
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 def create_project_hash
   # This just opens a file and reads it into a variable
   html = File.read('fixtures/kickstarter.html')
 
   kickstarter = Nokogiri::HTML(html)
+
+    projects = {} #an empty projects hash, which we will fill up with scraped data):
+
+    # Iterate through the projects
+kickstarter.css("li.project.grid_4").each do |project|
+  projects[project] = {}
 end
-binding.pry
+
+# return the projects hash
+projects
+end
+
