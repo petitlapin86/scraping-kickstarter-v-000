33cbaf68e067be7ba0444e815c1d952e40d1025e
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-21 18:52:17 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,12 +2,6 @@
 require 'pry'
 require 'nokogiri'
 
-# projects: kickstarter.css("li.project.grid_4")
-# title: project.css("h2.bbcard_name strong a").text
-# image link: project.css("div.project-thumbnail a img").attribute("src").value
-# description: project.css("p.bbcard_blurb").text
-# location: project.css("ul.project-meta span.location-name").text
-# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 def create_project_hash
   # This just opens a file and reads it into a variable
   html = File.read('fixtures/kickstarter.html')
@@ -26,9 +20,8 @@ def create_project_hash
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
   end
- 
+
 
 # return the projects hash
 projects
 end
-
