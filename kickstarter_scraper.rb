17c7eefa170dbf871860ddd8d89744cc0a5ca184
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-21 18:51:21 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -17,9 +17,10 @@ def create_project_hash
     projects = {} #an empty projects hash, which we will fill up with scraped data):
 
     # Iterate through the projects
-kickstarter.css("li.project.grid_4").each do |project|
-  projects[project] = {}
-end
+    kickstarter.css("li.project.grid_4").each do |project|
+      title = project.css("h2.bbcard_name strong a").text
+      projects[title.to_sym] = {}
+    end
 
 # return the projects hash
 projects
